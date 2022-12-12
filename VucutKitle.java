63f351bb44524f157be9d079596c558444253b88
İrem Rabia Topal <117042843+iremmr@users.noreply.github.com>
2022-12-12 22:56:06 +0300

    Add files via upload

---
+++ VucutKitle.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class VucutKitle {
+    public static void main(String[] args) {
+        double b,k,bmi;
+        Scanner i = new Scanner(System.in);
+        System.out.print("Boyunuzu m cinsinden giriniz: ");
+        b = i.nextDouble();
+
+        System.out.print("Kilonuzu kg cinsinden giriniz: ");
+        k = i.nextDouble();
+
+        bmi = k / (b * b);
+        System.out.println("Vücut Kitle İndeksiniz: " + bmi);
+
+
+    }
+}
